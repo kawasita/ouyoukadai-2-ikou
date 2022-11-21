ba78379664b37faecea5f54f877e8ce2ea1ccf1c
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-21 08:33:30 +0000

    コメント機能のフォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
 
   resources :books, only: [:new, :index,:show,:edit,:create,:destroy,:update] do
-    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create]
+      resource :favorites, only: [:create, :destroy]
   end
 
   resources :users, only: [:index,:show,:edit,:update, :destroy]
