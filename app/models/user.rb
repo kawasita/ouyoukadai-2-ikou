96802423c086c2cf422308d83f1e7d1efed4d1f7
kawasita <115086200+kawasita@users.noreply.github.com>
2022-11-23 04:14:38 +0000

    [Finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :follower, class_name: "Relationship", foreign_key: "user_id", dependent: :destroy
-  has_many :follow, through: :follower, source: :followed
+  # フォローするユーザーから見た中間テーブル
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされているユーザーから見た中間テーブル
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 中間テーブルactive_relationshipsを通って、フォローされる側(followed)を集める処理をfollowingsと命名
+  # フォローしているユーザーの情報がわかるようになる
+  has_many :followings, through: :active_relationships, source: :followed
+  # 中間テーブルpassive_relationshipsを通って、フォローする側(follower)を集める処理をfollowingsと命名
+  #　フォローされているユーザーの情報がわかるようになる
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -19,5 +27,17 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 2022_11_22_031856) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,4 +97,6 @@ ActiveRecord::Schema.define(version: 2022_11_22_031856) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "relationships", "users", column: "followed_id"
+  add_foreign_key "relationships", "users", column: "follower_id"
 end
