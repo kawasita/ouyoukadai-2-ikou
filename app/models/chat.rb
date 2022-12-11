e4adbebe65e1240fb398552aa4938d8e7cc7a981
kawasita <115086200+kawasita@users.noreply.github.com>
2022-12-11 10:30:03 +0000

    8a

---
+++ app/models/chat.rb
@@ -0,0 +1,5 @@
+class Chat < ApplicationRecord
+  belongs_to :user_params
+  belongs_to :room
+  validates :message,presence:true,length:{maximum:140}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "home/about" => "homes#about"
   get "search" => "searches#search"
+  get 'chat/:id' => 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
